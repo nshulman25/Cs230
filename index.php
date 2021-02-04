f1dd6f51fc6ec907aadffc148066686ffc718e34
nshulman25 <nshulman25@gmail.com>
2021-02-03 22:11:06 -0500

    Lab 2 about page

---
+++ index.php
@@ -0,0 +1,50 @@
+<main>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>Video Game Reviews (BETTER TITLE COMING SOON)</h1>
+        <p>This website will be used to review new video games that I have played recently. With in depth reviews on audio, graphics, preformace, and story.</p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/val.jpg">
+                <div class="container">
+                    <h2>Valorant</h2>
+                    <p>Review: </p>
+                    <p>Score: NA/10</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/cyber.jpg">
+                <div class="container">
+                    <h2>Cyberpunk 2077</h2>
+                    <p>Review: </p>
+                    <p>Score: NA/10</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/spider.jpg">
+                <div class="container">
+                    <h2>Spider-Man Miles Morales</h2>
+                    <p>Review: </p>
+                    <p>Score: NA/10</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/cod.jpg">
+                <div class="container">
+                    <h2>Call of Duty Black Ops Cold War</h2>
+                    <p>Review: </p>
+                    <p>Score: NA/10</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
